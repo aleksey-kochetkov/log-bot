b14d5ced1b1f2b927046858565ceea448ccee81f
akochetkov <akochetkov@ceki.ru>
2023-11-01 15:33:41 +0300

    43300 Вывод бота с логами ЛК РЦТ и Рейтинга на прод

--- src/main/java/ru/ceki/fgiski2/logbot/helper/ApplicationHelper.java
+++ src/main/java/ru/ceki/fgiski2/logbot/helper/ApplicationHelper.java
@@ -1,5 +1,6 @@
 package ru.ceki.fgiski2.logbot.helper;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,4 +8,12 @@ public class ApplicationHelper {
     private static final Logger LOGGER =
                         LoggerFactory.getLogger(ApplicationHelper.class);
     private static final String NAME = "log-bot.properties";
+    private static String logBotBotUsername;
+
+    static {
+    }
+
+    public static String getPropertyLogBotBotUsername() {
+        return logBotBotUsername;
+    }
 }
