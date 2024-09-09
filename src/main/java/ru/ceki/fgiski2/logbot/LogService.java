92649f06d8948317931963092643c5ba5b1d1759
akochetkov <akochetkov@ceki.ru>
2024-09-09 13:16:34 +0300

    61944 Доработка логов бота в части неавторизованных пользователей

--- src/main/java/ru/ceki/fgiski2/logbot/LogService.java
+++ src/main/java/ru/ceki/fgiski2/logbot/LogService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import ru.ceki.fgiski2.logbot.helper.NormalizedBlockingQueue;
 import ru.ceki.fgiski2.logbot.dto.QueueElement;
 
@@ -12,6 +13,10 @@ import ru.ceki.fgiski2.logbot.dto.QueueElement;
 public class LogService {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogService.class);
     private final Queue<QueueElement> queue = new NormalizedBlockingQueue();
+
+    public void setBotSession(BotSession botSession) {
+        this.botSession = botSession;
+    }
 
     public Queue<QueueElement> getQueue() {
         return this.queue;
