3665cf2549836ef3265ef26842303e4e33d27f8f
akochetkov <akochetkov@ceki.ru>
2024-10-03 16:24:54 +0300

    61944 Доработка логов бота в части неавторизованных пользователей

--- src/main/java/ru/ceki/fgiski2/logbot/LogService.java
+++ src/main/java/ru/ceki/fgiski2/logbot/LogService.java
@@ -1,6 +1,7 @@
 package ru.ceki.fgiski2.logbot;
 
 import java.util.Queue;
+import java.util.concurrent.CountDownLatch;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.generics.BotSession;
 import ru.ceki.fgiski2.logbot.helper.NormalizedBlockingQueue;
 import ru.ceki.fgiski2.logbot.dto.QueueElement;
+import ru.ceki.fgiski2.logbot.dto.ShutdownElement;
 
 @Service
 public class LogService {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogService.class);
+    private BotSession botSession;
+    private CountDownLatch running;
     private final Queue<QueueElement> queue = new NormalizedBlockingQueue();
+
+    public void start() {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::waitToShutdown));
+        this.running = new CountDownLatch(1);
+        new Thread(this::run).start();
+    }
+
+    public void run() {
+        try {
+            while (this.running.getCount() == 1) {
+                try {
+                    QueueElement element;
+                    element = this.queue.remove();
+                    if (element == QueueElement.SHUTDOWN) {
+                        this.running.countDown();
+                        this.processShutdown((ShutdownElement)element);
+                    } else if (element instanceof QueueElementImpl) {
+                        this.processElement((QueueElementImpl)element);
+                    }
+                } catch (Throwable throwable) {
+                    LOGGER.error(StringHelper.getStackTrace(throwable));
+                    ApplicationHelper.sleep(3000);
+                }
+            }
+        } catch (Exception exception) {
+            LOGGER.error(StringHelper.getStackTrace(exception));
+        } finally {
+            this.stopBotSession();
+            this.running.countDown();
+        }
+    }
+
+    public void waitToShutdown() {
+        this.suppressLogBotSelfReport = true;
+        try {
+            this.stopBotSession();
+            this.queue.add(QueueElement.SHUTDOWN);
+            if (this.executor instanceof ThreadPoolExecutor) {
+                ((ThreadPoolExecutor)this.executor).shutdown();
+            }
+            this.running.await(5, TimeUnit.SECONDS);
+            LOGGER.info("queue.size():{}", this.queue.size());
+        } catch (InterruptedException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
     public void setBotSession(BotSession botSession) {
         this.botSession = botSession;
