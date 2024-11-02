05bf63598963ed919bb06d2ecd3d22d64043eb67
akochetkov <akochetkov@ceki.ru>
2024-11-02 12:36:30 +0300

    61944 Доработка логов бота в части неавторизованных пользователей

--- src/main/java/ru/ceki/fgiski2/logbot/LogBot.java
+++ src/main/java/ru/ceki/fgiski2/logbot/LogBot.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.ceki.fgiski2.logbot.helper.ApplicationHelper;
 import ru.ceki.fgiski2.logbot.dto.QueueElement;
 import ru.ceki.fgiski2.logbot.dto.ShutdownElement;
